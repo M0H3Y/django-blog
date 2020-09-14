7da4d540eea6143002d74c68f4c16eb71180b088
root <root@localhost.localdomain>
2020-09-14 16:53:45 -0400

    First Commit

---
+++ users/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views #to route the requests to the views function
+
+urlpatterns = [
+]
---
+++ users/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+from django.contrib.auth.decorators import  login_required
+
+
+def register(request):
+    
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Your Account Has Been Created ! You Are Now Able To Log in")
+            return redirect("login")
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST,instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            messages.success(request, f"Your Account Has Been Updated!")
+            return redirect("profile")
+
+    else :
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'user_form':user_form,
+        'profile_form':profile_form
+    }
+    return render(request,'users/profile.html',context)
